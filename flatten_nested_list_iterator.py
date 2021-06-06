3c9b7a86e9d52bdd5b768b13369b4e88b4ddce14
SnehaPriya <31264991+snehasg95@users.noreply.github.com>
2021-06-06 12:16:44 -0700

    Create flatten_nested_list_iterator.py

---
+++ flatten_nested_list_iterator.py
@@ -0,0 +1,42 @@
+# In python iter() where we can pass a list, set, tuple will return an iterator object on it
+# Using this iterator object one can get the next element by calling next(iterator object of list)
+# hasNext is not available in python 
+  # for this purpose, simply check if the next(iterator obj on top of stack) is available -- if not it is fully processed, so pop it
+  
+  
+  # O(N) space for stack
+  # Time in constructor is constant
+  class NestedIterator:
+    
+    def __init__(self, nestedList):
+      
+      self.st = []
+      self.next_element = None
+      self.st.append(iter(nestedList))
+      
+      
+    def next(self): # O(1)
+      
+      return self.next_element.getInteger()
+    
+    
+    # 2 things - checks if there is a next element and also moves the iterator to next element to be processed
+    def hasNext(self): #O(depth of nestedlist)
+      
+      while self.st:
+        
+        try:
+          self.next_element = next(self.st[-1]) ## get the next element by using next(iter obj) -- this will return the obj if present
+          
+         except:
+          self.st.pop()
+          continue
+          
+          
+        if self.next_element.isInteger():
+          return True
+        
+        else:
+          self.st.append(iter(self.next_element.getList()))
+          
+      return False
